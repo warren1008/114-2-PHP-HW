61e8c83cb3d1f713e65c2138a99614011cf40871
warren1008 <A1123307@mail.nuk.edu.tw>
2026-03-27 01:11:04 +0800

    feat: implement Week 1 web programming assignments

---
+++ week_1/index.php
@@ -0,0 +1,21 @@
+<!--姓名和年稜撰寫練習-->
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>我的姓名及年齡</title>
+</head>
+
+<body>
+<?php
+$myname = "黃品勛";
+$en_name = "Charrlles wei";
+echo "我是" . $myname ."<br/>" . "英文名為:$en_name";
+?>
+<br/>
+<?php
+$i = 21;
+echo "現在的年紀是$i 歲，10年後我的年齡為 " . $i+10  . "歲<br/>"; 
+?>
+</body>
+</html>
